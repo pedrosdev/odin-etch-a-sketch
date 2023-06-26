69dfed0ef81a3864cdf1f0c2a673f550d4409ab7
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2023-06-26 16:51:41 -0300

    Allow creation of new grids

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-populateGrid(16);
+const newGridBtn = document.querySelector(".new-grid");
 
-function populateGrid(number) {
-  const grid = document.querySelector(".grid");
-  const gridWidth = grid.offsetWidth;
-  const gridHeight = grid.offsetHeight;
+newGridBtn.addEventListener("click", () => {
+  let gridSideLength = +prompt("Enter the length of each side of the grid (up to 100):", 16);
+
+  gridSideLength = (gridSideLength < 100) ? gridSideLength : 100;
+
+  createGrid(gridSideLength);
+})
+
+createGrid(16);
+
+function createGrid(number) {
+  const gridContainer = document.querySelector(".grid-container");
+  const gridTotalWidth = gridContainer.offsetWidth;
+  const gridTotalHeight = gridContainer.offsetHeight;
+
+  gridContainer.textContent = '';
 
   for (let i = 0; i < number; i++) {
     for (let j = 0; j < number; j++) {
       const square = document.createElement("div");
-      square.style.width = `${gridWidth / number}px`
-      square.style.height = `${gridHeight / number}px`;
+      square.style.width = `${gridTotalWidth / number}px`
+      square.style.height = `${gridTotalHeight / number}px`;
 
       square.addEventListener("mouseover", (e) => {
         e.target.style.backgroundColor = "#000000";
       })
 
-      grid.appendChild(square);
+      gridContainer.appendChild(square);
     }
   }
 }
